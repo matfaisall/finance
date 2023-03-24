16a94be914d89b1d5dceba1e41096f4c4d331e24
Muhammad Faisal <muh.faisal572@gmail.com>
2023-03-24 10:42:38 +0700

    make navigation bar and with responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,44 @@
-// import React from 'react'
+import React, { useState } from 'react'
+import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai'
 
-// const Navbar = () => {
-//   return (
-//     <div className="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white">
-//       <h1 className='w-full text-3xl font-bold text-[#00df9a]'>Finance</h1>
-//       <ul className="flex">
-//         <li className="p-4">Home</li>
-//         <li className="p-4">Company</li>
-//         <li className="p-4">Resources</li>
-//         <li className="p-4">About</li>
-//         <li className="p-4">Contact</li>
-//       </ul>
+const Navbar = () => {
+  const [nav, setNav] = useState(false);
 
-//     </div>
-//   )
-// }
+  const handleNav = () => {
+    setNav(!nav)
+  }
 
-// export default Navbar
+  return (
+    <div className="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white">
+      <h1 className='w-full text-3xl font-bold text-[#00df9a] '>Finance</h1>
+      <ul className="hidden md:flex">
+        <li className="p-4">Home</li>
+        <li className="p-4">Company</li>
+        <li className="p-4">Resources</li>
+        <li className="p-4">About</li>
+        <li className="p-4">Contact</li>
+      </ul>
+
+      {/* Mobile menu start*/}
+      <div onClick={handleNav} className='block md:hidden'>
+        {!nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+      </div>
+
+      <div className={!nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'fixed left-[-100%] ease-in-out'}>
+        <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>Finance</h1>
+        <ul className="p-4 uppercase">
+          <li className="p-4">Home</li>
+          <li className="p-4">Company</li>
+          <li className="p-4">Resources</li>
+          <li className="p-4">About</li>
+          <li className="p-4">Contact</li>
+        </ul>
+      </div>
+      {/* mobile menu end */}
+
+
+    </div>
+  )
+}
+
+export default Navbar
