3db02dda9cf828e62dafc4c461d93a732873a8cb
Muhammad Faisal <muh.faisal572@gmail.com>
2023-03-24 11:46:05 +0700

    make hero file dan implementation react-typed inside them

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import Hero from './components/Hero';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <Navbar />
+    <div>
+      <Navbar />
+      <Hero />
+    </div>
+
   );
 }
 
